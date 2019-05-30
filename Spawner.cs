b904c45000f92715e493b3b01231e184c88ff125
Martinmod <44831274+Martinmod@users.noreply.github.com>
2019-05-30 07:50:42 +0200

    Add files via upload
    Game files

---
+++ Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour{
+
+    public GameObject enemy;
+    public Transform[] spawnSpots;
+    private float TimeBtwSpawns;
+    public float startTimeBtwSpawns;
+
+    void Start(){
+        float timeBtwSpawns = startTimeBtwSpawns;
+    }
+
+    void Update(){
+        if (TimeBtwSpawns <= 0)
+        {
+            int randPos = Random.Range(0, spawnSpots.Length);
+            Instantiate(enemy, spawnSpots[randPos].position, Quaternion.identity);
+            TimeBtwSpawns = startTimeBtwSpawns;
+        }
+        else
+        {
+            TimeBtwSpawns -= Time.deltaTime;
+        }
+    }
+}
